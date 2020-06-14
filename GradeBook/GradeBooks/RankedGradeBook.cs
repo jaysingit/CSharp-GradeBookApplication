ee0f6d8a1f41227047297083bc98098444018056
Jayanth M <jayanth.mavilakkandy@ef.com>
2020-06-14 16:38:55 +0530

    Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,8 +12,27 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
-       
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+            else
+            {
+                if (averageGrade >= 80)
+                    return 'A';
+                else if (averageGrade >= 60)
+                    return 'B';
+                else if (averageGrade >= 40)
+                    return 'C';
+                else if (averageGrade >= 20)
+                    return 'D';
+            }
 
-       
+            return 'F';
+        }
+
+
+
+
     }
 }
